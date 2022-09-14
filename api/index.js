1808596b85f3041b4e32d9ae1bd7ca3ad30cd56d
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2022-09-14 10:58:48 -0400

    WIP

--- api/index.js
+++ api/index.js
@@ -1,23 +1,22 @@
+const db = require('database');
 const { success, error, empty, notFound } = require('./helpers/response');
 const route = require('./helpers/route');
 const actions = require('./actions');
 const { errors } = require('./helpers/messages');
 
-module.exports = (database) => {
-  return async (httpMethod, requestBody) => {
-    try {
-      switch (httpMethod.toUpperCase()) {
-        case 'OPTIONS': return empty();
-        case 'POST': return await route(database, actions, requestBody);
-        default: return error([errors.routing.nonPostRequest]);
-      }
-    } catch (error) {
-      console.error(error);
-
-      const isProduction = process.env.NODE_ENV === 'production';
-      const messages = isProduction ? [errors.default] : [errors.default, error.message];
-
-      return error(messages);
+module.exports = async (httpMethod, requestBody) => {
+  try {
+    switch (httpMethod.toUpperCase()) {
+      case 'OPTIONS': return empty();
+      case 'POST': return await route(db, actions, requestBody);
+      default: return error([errors.routing.nonPostRequest]);
     }
-  };
+  } catch (error) {
+    console.error(error);
+
+    const isProduction = process.env.NODE_ENV === 'production';
+    const messages = isProduction ? [errors.default] : [errors.default, error.message];
+
+    return error(messages);
+  }
 };
