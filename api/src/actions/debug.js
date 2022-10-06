43b33337a523e5695f8debf9d257b5714312ff2a
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2022-10-06 17:03:46 -0400

    Add tests

--- api/src/actions/debug.js
+++ api/src/actions/debug.js
@@ -2,8 +2,7 @@ const { success } = require('../methods/responses');
 
 exports = {
   authenticate: true,
-  execute: async (context, user, input) => {
-    const { now, env, db } = context;
+  execute: async ({ now, env, db }, user, input) => {
     const message = 'Debug info returned successfully.';
 
     return success({ now, env, user, input }, message);
---
+++ api/test/actions/debug.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+const { stub } = require('sinon');
+const proxyquire = require('proxyquire');
+
+describe('actions/debug.js', () => {
+  const successStub = stub();
+
+  const action = proxyquire('', {
+    '../methods/responses': {
+      success: successStub
+    }
+  });
+
+  beforeEach(() => {
+    successStub.reset();
+  });
+
+  describe('when initialized', () => {
+    it('should have the correct properties', () => {
+      expect(action.authenticate).to.eq(true);
+      expect(typeof action.execute).to.eq('function');
+    });
+  });
+
+  describe('when executed', () => {
+    it('should call success response method', async () => {
+      await action.execute({
+        now: Date.now(),
+        env: {},
+        db: {}
+      }, {}, {});
+
+      expect(successStub.called).to.eq(true);
+    });
+  });
+});
