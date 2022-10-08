866f3d01e80c292f399c0805da2c0fdf24426846
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2022-10-07 20:46:35 -0400

    WIP

--- client/.eleventy.js
+++ client/.eleventy.js
@@ -1,11 +1,13 @@
-// Tutorial:
-// https://www.youtube.com/watch?v=uzM5lETc6Sg
+// Tutorials:
+// #1: https://www.youtube.com/watch?v=uzM5lETc6Sg done!
+// #2: https://www.youtube.com/watch?v=WTVv5IbPN1k
+// #3: https://www.youtube.com/watch?v=ty0_xOxeRCU
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addPassthroughCopy('11-src/static');
+  eleventyConfig.addPassthroughCopy('11-src/assets');
 
   return {
     dir: {
-      input: '11-src',
+      input: '11-src/content',
       layouts: 'layouts',
       includes: 'includes',
       output: '11-dist'
---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './11-src/**/*.{html,css}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
