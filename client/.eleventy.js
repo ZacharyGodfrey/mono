fa5710890f9ef64402be899e47eaabf6309ece36
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2022-10-10 11:16:10 -0400

    WIP

--- client/.eleventy.js
+++ client/.eleventy.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs-extra');
+
 // Tutorials:
 // #1: https://www.youtube.com/watch?v=uzM5lETc6Sg done!
 // #2: https://www.youtube.com/watch?v=WTVv5IbPN1k
@@ -5,11 +8,16 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/static');
 
+  eleventyConfig.addFilter('asBase64', (fileName) => {
+    const filePath = path.resolve(__dirname, `./src/content/_includes/${fileName}`);
+
+    return fs.readFileSync(filePath, { encoding: 'base64' });
+  });
+
   return {
     dir: {
       input: 'src/content',
-      layouts: 'layouts',
-      includes: 'includes',
+      includes: '_includes',
       output: 'dist'
     },
     htmlTemplateEngine: 'njk',
